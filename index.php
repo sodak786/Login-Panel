09233167096fcc5e620e509671585a053bca463f
SodooSodastreamSodny <chingunsodoo786@gmail.com>
2024-03-19 21:07:23 +0100

    Revert "3"
    This reverts commit c6f0c89051d00c9a98c360ec8c08073ca0ae5815.

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-<?php
-
-session_start();
-
-if(!isset($_SESSION["loggedin"])) {
-    header("Location: /login.php");
-    exit;
-}
-
-?>
-
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <p>Tohle je index</p>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>login panel</title>
+        <link rel="stylesheet" href="login.css">
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <h1>Login</h1>
+            <?php if(isset($_GET['error'])){?>
+                <p class="error"><?php echo $_GET['error']; ?></p>
+            <?php } ?>
+            <p>Username</p>
+            <input type="text" name="uname" placeholder="username">
+            <p>Password</p>
+            <input type="password" name="password" placeholder="password">
+            <br>
+            <button type="submit">Login</button>
+        </form>
+    </body>
 </html>
--- login.php
+++ login.php
@@ -1,9 +1,6 @@
 <?php
 global $conn;
-session_start();
 include "db_conn.php";
-$error = null;
-
 if(isset($_POST['uname']) && isset($_POST['password'])){
     function validate($data){
         $data = trim($data);
@@ -22,22 +19,21 @@ if(isset($_POST['uname']) && isset($_POST['password'])){
         header("Location: index.php?error=Password is required");
         exit();
     }else{
-        $sql = "";
-        $stmt = $conn->prepare("SELECT * FROM users WHERE user_name = ? AND password=?");
-        $stmt->bind_param("ss", $uname, $pass);
+        $sql = "SELECT * FROM users WHERE user_name = '$uname' AND password='$pass'";
 
-        $result = $stmt->execute();
+        $result = mysqli_query($conn, $sql);
 
         if(mysqli_num_rows($result) === 1){
             $row = mysqli_fetch_assoc($result);
             if($row['user_name'] === $uname && $row['password'] === $pass){
-                $_SESSION["loggedin"] = true;
-                header("Location: /index.php");
+                echo "Logged in";
             }else{
-                $error = "Incorrect username or password";
+                header("Location: index.php?error=Incorrect username or password");
+                exit();
             }
         }else{
-            $error = "Incorrect username or password";
+            header("Location: index.php?error=Incorrect username or password");
+            exit();
         }
     }
 
@@ -45,32 +41,3 @@ if(isset($_POST['uname']) && isset($_POST['password'])){
     header("Location: index.php");
     exit();
 }
-
-?>
-
-
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>login panel</title>
-        <link rel="stylesheet" href="login.css">
-    </head>
-    <body>
-        <form method="post">
-            <h1>Login</h1>
-            <?php if(isset($error)){?>
-                <p class="error"><?php echo $error; ?></p>
-            <?php } ?>
-            <p>Username</p>
-            <input type="text" name="uname" placeholder="username">
-            <p>Password</p>
-            <input type="password" name="password" placeholder="password">
-            <br>
-            <button type="submit">Login</button>
-        </form>
-    </body>
-</html>
